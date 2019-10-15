03ec4d4c1c689c275cf06e4d8fe4651b6f04dd88
J.D. Hollis <jd@densityofspace.com>
2019-10-15 15:13:44 -0400

    Add iam:GetRole permission for ops role

--- service-example/pipeline/env-deployer-policy/main.tf
+++ service-example/pipeline/env-deployer-policy/main.tf
@@ -1,3 +1,8 @@
 data "aws_iam_policy_document" "env_deployer" {
+  statement {
+    actions   = ["iam:GetRole"]
+    resources = [var.ops_role_arn]
+  }
+
   # TODO: Insert deployer IAM permissions here.
 }
